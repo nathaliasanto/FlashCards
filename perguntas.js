a806dea0da97f48d7e251a19f50192d7de45c65a
nathaliasanto <151159814+nathaliasanto@users.noreply.github.com>
2024-11-29 20:13:59 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,20 @@
-
+criaCartao(
+    'Pensamento Computacional',
+    'O que é Pensamento Computacional?',
+    'O Pensamento computacional é uma forma de solução de problemas de forma rápida e eficaz.'
+)
+criaCartao(
+    'Pensamento Computacional',
+    'Quais os Pilares do Pensamento Computacional?',
+    'Os Pilares do Pensamento Computacional são: Decomposição, Reconhecimento de Padrões, Abstrações e Algorítimos.'
+)
+criaCartao(
+    'Pensamento Computacional',
+    'Quando Surgiu o Pensamento Computacional?',
+    'Surgiu no Século XIX, aproximadamente em 1960, em escritos de Papert e Solomon.'
+)
+criaCartao(
+    'Pensamento Computacional',
+    'Quem popularizou o termo Pensamento Computacional?',
+    'Jeannette Wing.'
+)
